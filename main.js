21d1f23fa7d3b65748d791e0685ffab8c91c886a
nataliaMykhailova <natalia.mykhailova.95@gmail.com>
2024-08-15 13:52:55 +0300

    commit10

--- main.js
+++ main.js
@@ -161,9 +161,9 @@ document.addEventListener("DOMContentLoaded", async function () {
                 const rentNearMeDiv = createCustomButton('Rent car near me', 'left', 'rent');
                 rentNearMeDiv.addEventListener('click', function (e) {
                     findNearestLocation(locations, inp);
-                    // document.querySelectorAll('#loc-icon path').forEach(function(icon) {
-                    //     icon.style.fill = 'black';
-                    // });
+                    document.querySelectorAll('#loc-icon path').forEach(function(icon) {
+                        icon.style.fill = 'black';
+                    });
                     closeAllLists();
                 });
                 a.appendChild(rentNearMeDiv);
@@ -172,9 +172,9 @@ document.addEventListener("DOMContentLoaded", async function () {
                 returnAtPickupDiv.addEventListener('click', function (e) {
                     inp.value = locationInput.value;
                     inp.closest('.input-wrapper').classList.remove('focused');
-                    // document.querySelectorAll('#drop-of-icon path').forEach(function(icon) {
-                    //     icon.style.fill = 'black';
-                    // });
+                    document.querySelectorAll('#drop-of-icon path').forEach(function(icon) {
+                        icon.style.fill = 'black';
+                    });
                     closeAllLists();
                 });
                 a.appendChild(returnAtPickupDiv);
@@ -193,18 +193,15 @@ document.addEventListener("DOMContentLoaded", async function () {
                     b.innerHTML += `<input type="hidden" value="${arr[i].display}">`;
                     b.addEventListener('click', function (e) {
                         inp.value = this.getElementsByTagName('input')[0].value;
-                        // if (inp.id === 'location') {
-                        //     document.querySelectorAll('#loc-icon path').forEach(function(icon) {
-                        //         icon.style.fill = 'black';
-                        //     });
-                        // } else if (inp.id === 'returnLocation') {
-                        //     document.querySelectorAll('#drop-of-icon path').forEach(function(icon) {
-                        //         icon.style.fill = 'black';
-                        //     });
-                        // }
-                        // document.querySelectorAll('.input-wrapper').forEach(function (element) {
-                        //     this.classList.remove('focused');
-                        // });
+                        if (inp.id === 'location') {
+                            document.querySelectorAll('#loc-icon path').forEach(function(icon) {
+                                icon.style.fill = 'black';
+                            });
+                        } else if (inp.id === 'returnLocation') {
+                            document.querySelectorAll('#drop-of-icon path').forEach(function(icon) {
+                                icon.style.fill = 'black';
+                            });
+                        }
                         closeAllLists();
 
 
